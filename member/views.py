417bdd8bf4c4afb9859f173c17cd4e505e20acaf
aswandhkolangarodi <aswandhk2000@gmail.com>
2022-08-11 11:45:42 +0530

    design completed

--- member/views.py
+++ member/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django import views
 from django.shortcuts import render
 
@@ -5,23 +6,35 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'member/index.html')
-
+    context ={
+        'is_index':True
+    }
+    return render(request, 'member/index.html', context)
+    
 
 def profile(request):
     return render(request, 'member/profile.html')
 
 def transactions(request):
-    return render(request, 'member/transactions.html')
+    context ={
+        'is_transactions':True
+    }
+    return render(request, 'member/transactions.html',context)
 
 def rewards(request):
-    return render(request, 'member/rewards.html')
+    context ={
+        'is_rewards':True
+    }
+    return render(request, 'member/rewards.html',context)
 
 def kyc_home(request):
     return render(request, 'member/kyc-home.html')
 
 def coin_details(request):
-    return render(request, 'member/coin-details.html')
+    context ={
+        'is_coin_details':True
+    }
+    return render(request, 'member/coin-details.html',context)
 
 def kyc(request):
     return render(request, 'member/kyc.html')
